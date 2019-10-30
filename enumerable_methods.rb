6c5678f1a12d5ae526fa662791a72bd6dabb1139
Vashira <vashimgs@gmail.com>
2019-10-30 14:16:41 +0000

    Added my-any and my-none

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -41,42 +41,105 @@ module Enumerable
     end
     true
   end
+
+  def my_any?
+    my_each do |i|
+      if block_given?
+        return true if yield(i)
+      else
+        return true until i
+      end
+    end
+    false
+  end
+
+  def my_none?
+    my_each do |i|
+      if block_given?
+        return false if yield(i)
+      else
+        return true until i
+      end
+    end
+    true
+  end
 end
 
+# test = [1, 4, 6, 7]
+# test.each do |x|
+#   puts x
+# end
 
+# test.my_each do |i|
+#   puts i
+# end
 
-test = [1, 4, 6, 7]
-test.each do |x|
-  puts x
-end
+# my_test = [11, 4, 5, 7, 8]
+# my_test.each do |t, y|
+#   puts " #{t} #{y} "
+# end
 
-test.my_each do |i|
-  puts i
-end
+# my_test.each_with_index do |s, l|
+#   puts "#{s} #{l}"
+# end
+# my_test.my_each_with_index do |i, k|
+#   puts " #{i} #{k} "
+# end
 
-my_test = [11, 4, 5, 7, 8]
-my_test.each do |t, y|
-  puts " #{t} #{y} "
-end
+# puts [1, 2, 3, 4, 5].select(&:even?) #=> [2, 4]
 
-my_test.my_each_with_index do |i, k|
-  puts " #{i} #{k} "
-end
+# puts [1, 2, 3, 4, 5].my_select(&:even?) #=> [2, 4]
 
-puts [1, 2, 3, 4, 5].select(&:even?) #=> [2, 4]
+# puts %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
+# puts %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
+# puts %w[ant bear cat].all?(/t/) #=> false
+# puts [1, 2i, 3.14].all?(Numeric) #=> true
+# puts [nil, true, 99].all? #=> false
+# puts [].all? #=> true
 
-puts [1, 2, 3, 4, 5].my_select(&:even?) #=> [2, 4]
+# puts '==========================================='
 
-%w[ant bear cat].all? { |word| word.length >= 3 } #=> true
-%w[ant bear cat].all? { |word| word.length >= 4 } #=> false
-%w[ant bear cat].all?(/t/) #=> false
-[1, 2i, 3.14].all?(Numeric) #=> true
-[nil, true, 99].all? #=> false
-[].all? #=> true
+# puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+# puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+# # puts %w[ant bear cat].my_all?(/t/) #=> false
+# # puts [1, 2i, 3.14].my_all?(Numeric) #=> true
+# puts [nil, true, 99].my_all? #=> false
+# puts [].my_all? #=> true
 
-puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
-# puts %w[ant bear cat].my_all?(/t/) #=> false
-# puts [1, 2i, 3.14].my_all?(Numeric) #=> true
-puts [nil, true, 99].my_all? #=> false
-puts [].my_all? #=> true
+# puts '============================================='
+# puts '============================================='
+# puts %w[ant bear cat].any? { |word| word.length >= 3 } #=> true
+# puts %w[ant bear cat].any? { |word| word.length >= 4 } #=> true
+# puts %w[ant bear cat].any?(/d/) #=> false
+# puts [nil, true, 99].any?(Integer) #=> true
+# puts [nil, true, 99].any? #=> true
+# puts [].any? #=> false
+
+# puts '============================================='
+# puts '============================================='
+# puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+# puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+# # puts %w[ant bear cat].my_any?(/d/) #=> false
+# # puts [nil, true, 99].my_any?(Integer) #=> true
+# puts [nil, true, 99].my_any? #=> true
+# puts [].my_any? #=> false
+
+puts '==================================================='
+puts %w[ant bear cat].none? { |word| word.length == 5 } #=> true
+puts %w[ant bear cat].none? { |word| word.length >= 4 } #=> false
+# %w{ant bear cat}.none?(/d/)                        #=> true
+# [1, 3.14, 42].none?(Float)                         #=> false
+puts [].none? #=> true
+puts [nil].none? #=> true
+puts [nil, false].none? #=> true
+puts [nil, false, true].none? #=> false
+
+puts '==================================================='
+puts %w[ant bear cat].my_none? { |word| word.length == 5 } #=> true
+puts %w[ant bear cat].my_none? { |word| word.length >= 4 } #=> false
+# %w{ant bear cat}.none?(/d/)                        #=> true
+# [1, 3.14, 42].none?(Float)                         #=> false
+puts [].my_none? #=> true
+puts [nil].my_none? #=> true
+puts [nil, false].my_none? #=> true
+puts [nil, false, true].my_none? #=> false
