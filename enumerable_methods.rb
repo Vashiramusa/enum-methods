9f90198077f95aa700e25a0f437767fb0980a639
Vashira <vashimgs@gmail.com>
2019-11-01 15:18:34 +0000

    added updated to my-all, my-any and my-none

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -34,54 +34,48 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
 
   # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def my_all?(arg = nil)
-    result = true
-    my_each do |element|
-      if result
-        if block_given?
-          result = false unless yield(element)
-        elsif arg
-          result = element.is_a?(arg) unless arg.is_a?(Regexp)
-          result = element.match?(arg) if arg.is_a?(Regexp)
-        else
-          result = false unless element
-        end
-      end
+    if block_given?
+      my_each { |element| return false unless yield(element) }
+    elsif arg.class == Class
+      my_each { |element| return false unless element.class.ancestors.include? arg }
+    elsif arg.class == Regexp
+      my_each { |element| return false unless element =~ arg }
+    elsif arg.nil?
+      my_each { |element| return false unless element }
+    else
+      my_each { |element| return false unless element == arg }
     end
-    result
+    true
   end
 
   def my_any?(arg = nil)
-    result = false
-    my_each do |element|
-      if result == false
-        if block_given?
-          result = yield(element)
-        elsif arg
-          result = element.is_a?(arg) unless arg.is_a?(Regexp)
-          result = element.match?(arg) if arg.is_a?(Regexp)
-        else
-          result = true unless element
-        end
-      end
+    if block_given?
+      my_each { |element| return true if yield element }
+    elsif arg.class == Class
+      my_each { |element| return true if element.class.ancestors.include? arg }
+    elsif arg.class == Regexp
+      my_each { |element| return true if arg =~ element }
+    elsif arg.nil?
+      my_each { |element| return true if element }
+    else
+      my_each { |element| return true if element == arg }
     end
-    result
+    false
   end
 
   def my_none?(arg = nil)
-    result = true
-    my_each do |element|
-      if result
-        if block_given?
-          result = false if yield(element)
-        elsif arg
-          result = !element.is_a?(arg) unless arg.is_a?(Regexp)
-          result = !element.match?(arg) if arg.is_a?(Regexp)
-        elsif element
-          result = false
-        end
-      end
+    if block_given?
+      my_each { |element| return false if yield element }
+    elsif arg.class == Class
+      my_each { |element| return false if element.class.ancestors.include? arg }
+    elsif arg.class == Regexp
+      my_each { |element| return false if arg =~ element }
+    elsif arg.nil?
+      my_each { |element| return false if element }
+    else
+      my_each { |element| return false if element == arg }
     end
-    result
+    true
   end
 
   def my_count(arg = nil)
